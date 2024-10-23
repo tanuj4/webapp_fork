e4523691a4fce3dfafaf5a63f9e5f234dd9a9c6d
Tanuj Kodali <tanujkodali@Tanujs-MacBook-Pro-2.local>
2024-10-23 17:59:44 -0400

    pushing script changes for file security

--- scripts/webapp.sh
+++ scripts/webapp.sh
@@ -18,6 +18,9 @@ sudo npm install --prefix /home/csye6225/webapp/
 sudo chown -R csye6225:csye6225 /home/csye6225/webapp/
 
 
+sudo find /home/csye6225/webapp/ -type d -exec chmod 755 {} \;
+
+
 sudo cp /home/csye6225/webapp/webapp.service /etc/systemd/system/webapp.service
 
 
